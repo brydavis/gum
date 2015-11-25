e68becaedd951fb17cdb31767942fe62d412ffd4
Bryan Davis <bryan@stax.io>
2015-11-24 17:55:07 -0800

    Update encode.go

--- encode.go
+++ encode.go
@@ -1,44 +1,23 @@
 package main
 
 import (
-	// "flag"
 	"bytes"
 	"fmt"
 	"image"
-	// "image/color"
-	// "image/draw"
 	"image/gif"
 	"image/jpeg"
-
-	// "io"
 	"io/ioutil"
-	// "math/rand"
 	"os"
 	"path"
-	// "time"
 )
 
 func main() {
-	// flag.Parse()
-	// rand.Seed(time.Now().UTC().UnixNano())
+
 	out, err := os.Create("./output.gif")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	} // generate some QR code look a like image
-
-	// imgRect := image.Rect(0, 0, 100, 100)
-	// img := image.NewGray(imgRect)
-	// draw.Draw(img, img.Bounds(), &image.Uniform{color.White}, image.ZP, draw.Src)
-	// for y := 0; y < 100; y += 10 {
-	// 	for x := 0; x < 100; x += 10 {
-	// 		fill := &image.Uniform{color.Black}
-	// 		if rand.Intn(10)%2 == 0 {
-	// 			fill = &image.Uniform{color.White}
-	// 		}
-	// 		draw.Draw(img, image.Rect(x, y, x+10, y+10), fill, image.ZP, draw.Src)
-	// 	}
-	// }
+	}
 
 	dir, _ := ioutil.ReadDir(".")
 
@@ -74,8 +53,7 @@ func main() {
 	g.Delay = []int{100, 100, 100}
 	g.LoopCount = 1000
 
-	// you can add more parameters if you want // ok, write out the data into the new GIF fil
-	err = gif.EncodeAll(out, g) // put num of colors to 256 <------- here
+	err = gif.EncodeAll(out, g)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
